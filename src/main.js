c265073b54e36922ac58ab9bc74455daf99fb289
Matheus Arakaki <arakakimath@gmail.com>
2024-10-08 12:12:14 -0400

    reorganizing folders

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@
 // CSS
 /*import "./styles/global.css"
 console.log("olá")*/
-import "./scripts/header-date.js"
-import "./scripts/phone-number.js"
+import "./scripts/form/header-date.js"
+import "./scripts/form/phone-number.js"
 import "./scripts/buttons/button-new-schedule.js"
 import "./scripts/buttons/cancel-scheduling.js"
 import "./scripts/form/date-time.js"
